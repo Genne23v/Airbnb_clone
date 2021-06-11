f2c92e26693aea0533b9be2d8ff130266bef8fcd
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-06-11 13:22:44 -0400

    working in progress

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const HTTP_PORT = process.env.PORT || 8080;
 const express = require('express'),
     app = express();
 
+app.use(express.static('static'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to my home!');
 });
 
-app.get('/myimage', (req, res) => {
-    res.send('This is my image page');
+app.get('/registration', (req, res) => {
+    res.send('This is my registration page');
 });
 
-app.get('/info', (req, res) => {
-    res.send('This is my info page');
+app.get('/room_listing', (req, res) => {
+    res.send('This is the result of your search');
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
 })
 
 app.listen(HTTP_PORT, () => {
     console.log(`The server is running... to port ${HTTP_PORT}`);
 });
+
+

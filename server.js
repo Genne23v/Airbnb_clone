868d45098a8964898da6d27879119598fc7cd01f
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-06-15 01:21:56 -0400

    assign1 initial complete

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
 const HTTP_PORT = process.env.PORT || 8080;
-const express = require('express'),
-    handlebars = require('express-handlebars'),
-    path = require('path'),
-    app = express();
+const express = require("express"),
+  handlebars = require("express-handlebars"),
+  path = require("path"),
+  app = express();
 
-app.use(express.static('public'));
-app.set('view engine', 'hbs');    
-app.engine('hbs', handlebars( { extname: 'hbs', defaultLayout: 'home', layoutDir: __dirname + '/views/layouts'}));
+app.use(express.static("public"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutDir: __dirname + "/views/layouts",
+    partialsDir: path.join(__dirname, "/views/partials"),
+  })
+);
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        layout: 'index'
-    });
+app.get("/", (req, res) => {
+  res.render("main", {
+    layout: false,
+  });
 });
 
-app.get('/registration', (req, res) => {
-    res.render('registration', {
-        layout: false
-    });
+app.get("/room-listing", (req, res) => {
+  res.render("room-listing", {
+    layout: false,
+  });
 });
 
-app.get('/room-listing', (req, res) => {
-    res.render('room-listing', {
-        layout: 'index'
-    });;
+app.get("/practice", (req, res) => {
+  res.render("_main", {
+    layout: false,
+  });
 });
-
 app.use((req, res) => {
-    res.status(404).send("Page Not Found");
-})
+  res.status(404).send("Page Not Found");
+});
 
 app.listen(HTTP_PORT, () => {
-    console.log(`The server is running... to port ${HTTP_PORT}`);
+  console.log(`The server is running... to port ${HTTP_PORT}`);
 });
-
-
